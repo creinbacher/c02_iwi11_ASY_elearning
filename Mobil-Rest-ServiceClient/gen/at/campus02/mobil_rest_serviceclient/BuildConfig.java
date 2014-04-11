0300818096e3747e064d4618972974d66dbda551
creinbacher <reinbacher@gmail.com>
2014-04-11 16:58:46 +0200

    removed

--- Mobil-Rest-ServiceClient/gen/at/campus02/mobil_rest_serviceclient/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package at.campus02.mobil_rest_serviceclient;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
